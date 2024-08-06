3a603c6a5105cf6acb4311b70767e3510b6d01c2
Jake Spievak <jakespvk@gmail.com>
2024-08-05 20:59:04 -0700

    hopefully make faster and WEZ IS AWESOME CURSOR COLOR IS GREAT NOW

--- wezterm.lua
+++ wezterm.lua
@@ -1,9 +1,5 @@
--- Pull in the wezterm API
 local wezterm = require 'wezterm'
-
--- This table will hold the configuration.
 local config = {}
-
 config.keys = {
     {
         key = 'F2',
@@ -11,26 +7,24 @@ config.keys = {
         action = wezterm.action.ReloadConfiguration,
     },
 }
-
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
 if wezterm.config_builder then
   config = wezterm.config_builder()
 end
-
--- This is where you actually apply your config choices
-
--- GitHub Dark/Wez best for Gruvbox Vim, while Snazzy best for Rose-Pine & TokyoNight
---config.color_scheme = 'GitHub Dark'
 --config.color_scheme = 'Wez (Gogh)'
 config.color_scheme = 'Snazzy (Gogh)'
---config.color_scheme = 'GruvboxDarkHard'
-
-
-config.window_background_opacity = 0.95
-config.font = wezterm.font('Iosevka', {weight='Light'})
-config.font_size = 16
-config.enable_tab_bar = false
-
--- and finally, return the configuration to wezterm
+--config.default_cursor_style = 'SteadyBar'
+config.cursor_thickness = "1pt"
+config.max_fps = 165
+config.window_background_opacity = 0.80
+config.font = wezterm.font('Iosevka', {weight='Regular'})
+config.font_size = 15
+--config.dpi = 196.0
+--config.enable_tab_bar = false
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
+config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+config.force_reverse_video_cursor = true
+wezterm.action.SpawnCommandInNewWindow {
+    args = { 'tmux' }
+}
 return config
